b9d4443769705f6a3f3970ce23f909bd5428c83e
at0misk <ktp925@gmail.com>
2017-05-12 16:04:15 -0700

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_cable.url = "ws://35.167.229.106/cable"
+  config.action_cable.url = "/cable"
   ActionCable.server.config.disable_request_forgery_protection = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
