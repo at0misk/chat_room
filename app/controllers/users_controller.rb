331afe9aac027e84fa1b3973b989563a9c512db0
at0misk <ktp925@gmail.com>
2017-05-12 11:18:44 -0700

    crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,19 @@ class UsersController < ApplicationController
 			redirect_to '/chatroom'
 		end
 	end
+	def edit
+		@user = User.find(session[:user_id])
+	end
 	def user_params
 		params.require(:user).permit(:user_name, :password)
 	end
+   	def update
+  		@user = User.find(session[:user_id])
+	    if @user.update(user_params)
+	    	flash[:errors] = nil
+	    else
+	    	flash[:errors] = @user.errors.full_messages
+	    end
+	    redirect_to :back
+  	end
 end
