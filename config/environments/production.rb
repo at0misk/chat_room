8f9380cbbbd3bfa98d8c21f9d9731a0ce9f8f3b1
at0misk <ktp925@gmail.com>
2017-05-12 15:53:48 -0700

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_cable.url = "ws://35.167.229.106/:2000cable"
+  config.action_cable.url = "ws://35.167.229.106/:3000cable"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
