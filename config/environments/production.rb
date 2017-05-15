7474a2bce363b73dc71b15751aca003b1a45150c
at0misk <ktp925@gmail.com>
2017-05-15 11:00:09 -0700

    ac

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_cable.url = "ws://35.163.87.222:3000/cable"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  ActionCable.server.config.disable_request_forgery_protection = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
