4a48487abe36935df320d2c6613d08497778fb8d
at0misk <ktp925@gmail.com>
2017-05-15 11:44:15 -0700

    ac

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,22 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-  	def connect
-  	end
-  end
+    class Connection < Action Cable::Connection::Base
+      # uniquely identify this connection
+      identified_by :current_user
+
+      # called when the client first connects
+      def connect
+        self.current_user = find_verified_user
+      end
+
+      protected
+        def find_verified_user
+          # session isn't accessible here
+          if current_user = User.find(cookies.signed[:user_id])
+            current_user
+          else
+            # writes a log and raises an exception
+            reject_unauthorized_connection
+          end
+        end
+    end
 end
