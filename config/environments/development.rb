356726940d987b368b07d5217d8180102f521854
at0misk <ktp925@gmail.com>
2017-05-15 13:53:37 -0700

    ac

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  # config.action_cable.url = "ws://35.163.87.222/cable"
   config.action_cable.url = "/cable"
   config.action_cable.allowed_request_origins = ['http://35.163.87.222']
   ActionCable.server.config.disable_request_forgery_protection = true
