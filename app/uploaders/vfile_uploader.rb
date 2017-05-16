d63602e541c0082544ea0599efa1d25d0af9aa68
at0misk <ktp925@gmail.com>
2017-05-16 14:27:12 -0700

    uploader

--- app/uploaders/vfile_uploader.rb
+++ app/uploaders/vfile_uploader.rb
@@ -11,7 +11,7 @@ class VfileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{Rails.roo}/videos/"
+    "#{Rails.root}/videos/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
