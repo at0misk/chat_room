08e52dd29a9ba4600fee35a00a0f0a126d226428
at0misk <ktp925@gmail.com>
2017-05-16 12:00:36 -0700

    switched to postgres

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -49,6 +49,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+gem 'pg'
 
 gem 'carrierwave', '~> 1.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170516163715) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20170516163715) do
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_messages_on_topic_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.index ["topic_id"], name: "index_messages_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "topics", force: :cascade do |t|
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170516163715) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_topics_on_user_id"
+    t.index ["user_id"], name: "index_topics_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,7 +55,11 @@ ActiveRecord::Schema.define(version: 20170516163715) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.string   "vfile"
-    t.index ["category_id"], name: "index_videos_on_category_id"
+    t.index ["category_id"], name: "index_videos_on_category_id", using: :btree
   end
 
+  add_foreign_key "messages", "topics"
+  add_foreign_key "messages", "users"
+  add_foreign_key "topics", "users"
+  add_foreign_key "videos", "categories"
 end
